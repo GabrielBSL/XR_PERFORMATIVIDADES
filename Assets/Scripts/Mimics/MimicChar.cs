62d7e13e77bad2f6a45dbf06a06fd52c418ff80c
Gabriel Barroso da Silva Lima <gabriel.barroso.am@gmail.com>
2024-02-17 10:09:44 -0300

    Hand movement smoothness
    Hand movement smoothness

--- Assets/Scripts/Mimics/MimicChar.cs
+++ Assets/Scripts/Mimics/MimicChar.cs
@@ -1,13 +1,28 @@
 using Main.Events;
+using System;
+using System.Collections;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 namespace Main.Mimics
 {
     public class MimicChar : MonoBehaviour
     {
+        private struct HandInfo
+        {
+            public Vector3 localEulerAngles;
+            public Vector3 localPosition;
+            public Vector3 globalPosition;
+        }
+
         [SerializeField] private Transform rightArmTarget;
         [SerializeField] private Transform leftArmTarget;
+
+        [Header("Movement")]
+        [SerializeField] private Transform rightHandDestination;
+        [SerializeField] private Transform leftHandDestination;
+        [SerializeField, Range(.01f, 1f)] private float handMovementSmoothness = .1f;
+        [SerializeField] private Vector2 movementDeltaVariation = Vector2.one;
+        [SerializeField] private float movementDelay = .5f;
 
         private Transform rightArmTargetReference;
         private Transform leftArmTargetReference;
@@ -15,9 +30,6 @@ namespace Main.Mimics
 
         private Vector3 _lastRightHandLocalPos;
         private Vector3 _lastLeftHandLocalPos;
-
-        private Vector3 _lastRightHandGlobalPos;
-        private Vector3 _lastLeftHandGlobalPos;
 
         private void OnEnable()
         {
@@ -31,20 +43,19 @@ namespace Main.Mimics
             MainEventsManager.leftHandTransformUpdate -= ReceiveLeftArmTarget;
             MainEventsManager.bodyTransformUpdate -= ReceiveBodyReference;
         }
-
         private void ReceiveRightArmTarget(Transform _rightArmTarget)
         {
             rightArmTargetReference = _rightArmTarget;
-            rightArmTarget.localPosition = _rightArmTarget.localPosition;
             _lastRightHandLocalPos = _rightArmTarget.localPosition;
-            _lastRightHandGlobalPos = _rightArmTarget.position;
+            rightArmTarget.localPosition = _rightArmTarget.localPosition;
+            rightHandDestination.localPosition = _rightArmTarget.localPosition;
         }
         private void ReceiveLeftArmTarget(Transform _leftArmTarget)
         {
             leftArmTargetReference = _leftArmTarget;
-            leftArmTarget.localPosition = _leftArmTarget.localPosition;
             _lastLeftHandLocalPos = _leftArmTarget.localPosition;
-            _lastLeftHandGlobalPos = _leftArmTarget.position;
+            leftArmTarget.localPosition = _leftArmTarget.localPosition;
+            leftHandDestination.localPosition = _leftArmTarget.localPosition;
         }
         private void ReceiveBodyReference(Transform _bodyReference)
         {
@@ -54,63 +65,103 @@ namespace Main.Mimics
         // Update is called once per frame
         void Update()
         {
+            //StartCoroutine(DelayAction());
+            HandInfo rightHandInfo = new HandInfo()
+            {
+                localEulerAngles = rightArmTargetReference.localEulerAngles,
+                localPosition = rightArmTargetReference.localPosition,
+                globalPosition = rightArmTargetReference.position,
+            };
+            HandInfo leftHandInfo = new HandInfo()
+            {
+                localEulerAngles = leftArmTargetReference.localEulerAngles,
+                localPosition = leftArmTargetReference.localPosition,
+                globalPosition = leftArmTargetReference.position,
+            };
+
+            Vector3 bodyReferencePosition = bodyReference.position;
+
             //body Height
-            transform.position = new Vector3(transform.position.x, bodyReference.position.y, transform.position.z);
+            transform.position = new Vector3(transform.position.x, bodyReferencePosition.y, transform.position.z);
 
             //body Rotation
-            Vector3 direction = bodyReference.position - transform.position;
+            Vector3 direction = bodyReferencePosition - transform.position;
             Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
             newRotation.x = transform.rotation.x;
             newRotation.z = transform.rotation.z;
             transform.rotation = newRotation;
 
-            //Hands angles
-            rightArmTarget.localEulerAngles = rightArmTargetReference.localEulerAngles;
-            leftArmTarget.localEulerAngles = leftArmTargetReference.localEulerAngles;
+            SetHandsRotationAndPosition(rightHandInfo, leftHandInfo);
 
-            //Hands positions
-            //Right hand movements of the player are translated to left hand of the mimic for the "Mirror" effect
-            Vector3 rightHandLocalDelta = rightArmTargetReference.localPosition - _lastRightHandLocalPos;
-            Vector3 leftHandLocalDelta = leftArmTargetReference.localPosition - _lastLeftHandLocalPos;
-
-            Vector3 rightHandGlobalDelta = rightArmTargetReference.position - _lastRightHandGlobalPos;
-            Vector3 leftHandGlobalDelta = leftArmTargetReference.position - _lastLeftHandGlobalPos;
-
-            Vector2 mimicFacingDir = new Vector2(transform.forward.x, transform.forward.z);
-            Vector2 referenceFacingDir = new Vector2(bodyReference.forward.x, bodyReference.forward.z);
-
-            Vector2 mimicRightHandDelta = RotateVector(referenceFacingDir, mimicFacingDir, rightHandLocalDelta, rightHandGlobalDelta);
-            Vector2 mimicLeftHandDelta = RotateVector(referenceFacingDir, mimicFacingDir, leftHandLocalDelta, leftHandGlobalDelta);
-
-            rightArmTarget.localPosition += new Vector3(mimicRightHandDelta.x, 0, mimicRightHandDelta.y) * 2;
-            leftArmTarget.localPosition += new Vector3(mimicLeftHandDelta.x, 0, mimicLeftHandDelta.y) * 2;
-
-            _lastRightHandLocalPos = rightArmTargetReference.localPosition;
-            _lastLeftHandLocalPos = leftArmTargetReference.localPosition;
-
-            _lastLeftHandGlobalPos = leftArmTargetReference.position;
-            _lastRightHandGlobalPos = rightArmTargetReference.position;
+            MoveToDestination();
         }
 
-        public Vector2 RotateVector(Vector2 dirInitial, Vector2 dirFinal, Vector3 localDelta, Vector3 globalDelta)
+        private void MoveToDestination()
         {
-            Vector2 local2DDelta = new Vector2(localDelta.x, localDelta.z);
+            rightArmTarget.localPosition = Vector3.Lerp(rightArmTarget.localPosition, rightHandDestination.localPosition, handMovementSmoothness);
+            leftArmTarget.localPosition = Vector3.Lerp(leftArmTarget.localPosition, leftHandDestination.localPosition, handMovementSmoothness);
+        }
 
-            if(local2DDelta == Vector2.zero)
+        private IEnumerator DelayAction()
+        {
+            HandInfo rightHandInfo = new HandInfo()
             {
-                return Vector2.zero;
-            }
+                localEulerAngles = rightArmTargetReference.localEulerAngles,
+                localPosition = rightArmTargetReference.localPosition,
+                globalPosition = rightArmTargetReference.position,
+            };
+            HandInfo leftHandInfo = new HandInfo()
+            {
+                localEulerAngles = leftArmTargetReference.localEulerAngles,
+                localPosition = leftArmTargetReference.localPosition,
+                globalPosition = leftArmTargetReference.position,
+            };
 
-            Vector2 global2DDelta = new Vector2(globalDelta.x, globalDelta.z);
+            Vector3 bodyReferencePosition = bodyReference.position;
 
-            // Calcula o ângulo de rotação entre dirInitial e dirFinal
-            float angle = Vector2.SignedAngle(dirInitial, dirFinal);
+            yield return new WaitForSeconds(movementDelay);
 
-            // Rotaciona dirProduct pelo ângulo calculado
-            Quaternion rotation = Quaternion.Euler(0, 0, angle);
-            Vector2 rotatedDirProduct = rotation * global2DDelta;
+            //body Height
+            transform.position = new Vector3(transform.position.x, bodyReferencePosition.y, transform.position.z);
 
-            return rotatedDirProduct;
+            //body Rotation
+            Vector3 direction = bodyReferencePosition - transform.position;
+            Quaternion newRotation = Quaternion.LookRotation(direction, Vector3.up);
+            newRotation.x = transform.rotation.x;
+            newRotation.z = transform.rotation.z;
+            transform.rotation = newRotation;
+
+            SetHandsRotationAndPosition(rightHandInfo, leftHandInfo);
+        }
+
+        private void SetHandsRotationAndPosition(HandInfo rightHandInfo, HandInfo leftHandInfo)
+        {
+            //Hands angles
+            rightArmTarget.localEulerAngles = leftHandInfo.localEulerAngles - new Vector3(0, 0, 180);
+            leftArmTarget.localEulerAngles = rightHandInfo.localEulerAngles - new Vector3(0, 0, 180);
+
+            Vector3 rhiLp = rightHandInfo.localPosition;
+            Vector3 lhiLp = leftHandInfo.localPosition;
+            Vector3 rhdLp = rightHandDestination.localPosition;
+            Vector3 lhdLp = leftHandDestination.localPosition;
+
+            Vector3 rightHandLocalDelta = rhiLp - _lastRightHandLocalPos;
+            Vector3 leftHandLocalDelta = lhiLp - _lastLeftHandLocalPos;
+
+            float randomRightDeltaMultiplier = UnityEngine.Random.Range(movementDeltaVariation.x, movementDeltaVariation.y);
+            float randomLeftDeltaMultiplier = UnityEngine.Random.Range(movementDeltaVariation.x, movementDeltaVariation.y);
+
+            rightHandLocalDelta = new Vector3(-rightHandLocalDelta.x, 0, rightHandLocalDelta.z) * randomRightDeltaMultiplier;
+            leftHandLocalDelta = new Vector3(-leftHandLocalDelta.x, 0, leftHandLocalDelta.z) * randomLeftDeltaMultiplier;
+
+            rightHandDestination.localPosition = new Vector3(rhdLp.x, lhiLp.y, rhdLp.z) + leftHandLocalDelta;
+            leftHandDestination.localPosition = new Vector3(lhdLp.x, rhiLp.y, lhdLp.z) + rightHandLocalDelta;
+
+            _lastRightHandLocalPos = rhiLp;
+            _lastLeftHandLocalPos = lhiLp;
+
+            //Debug.Log(new Vector3(rhiLp.x, rhiLp.y, rhiLp.z) + " + " + rightHandLocalDelta + " = " + (new Vector3(rhiLp.x, rhiLp.y, rhiLp.z) + rightHandLocalDelta));
+            //Debug.Log(rhdLp + " - " + rightHandLocalDelta + " - " +_lastRightHandLocalPos);
         }
     }
 }
