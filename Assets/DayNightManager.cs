7d49dc3475ba5fc3e73ab671ce1b418c50997b34
TucoErthal <tucoerthal@gmail.com>
2024-10-16 09:59:55 -0300

    backup

--- Assets/DayNightManager.cs
+++ Assets/DayNightManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class DayNightManager : MonoBehaviour
 {
+    [SerializeField] private bool useDynamicLighting;
     [SerializeField] [Range(0f, 1f)] private float time;
     [SerializeField] private Light directionalLight;
     [SerializeField] private Quaternion lightRotationDay;
@@ -14,12 +15,18 @@ public class DayNightManager : MonoBehaviour
     [SerializeField] private Material skyboxMaterial;
     [SerializeField] private Color fogColorDay;
     [SerializeField] private Color fogColorNight;
+    [SerializeField] private float fogDensityDay;
+    [SerializeField] private float fogDensityNight;
 
     void Update()
     {
         skyboxMaterial.SetFloat("_time", time);
-        directionalLight.transform.rotation = Quaternion.Slerp(lightRotationDay, lightRotationNight, time);
-        directionalLight.colorTemperature = Mathf.Lerp(lightColorTemperatureDay, lightColorTemperatureNight, time);
+        if(useDynamicLighting)
+        {
+            directionalLight.transform.rotation = Quaternion.Slerp(lightRotationDay, lightRotationNight, time);
+            directionalLight.colorTemperature = Mathf.Lerp(lightColorTemperatureDay, lightColorTemperatureNight, time);
+        }
         RenderSettings.fogColor = Color.Lerp(fogColorDay, fogColorNight, time);
+        RenderSettings.fogDensity = Mathf.Lerp(fogDensityDay, fogDensityNight, time);
     }
 }
