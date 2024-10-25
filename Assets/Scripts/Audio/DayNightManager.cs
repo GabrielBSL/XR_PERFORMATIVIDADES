7d21314a37390eb3847c8a305f6c12ade3ee560d
TucoErthal <tucoerthal@gmail.com>
2024-10-24 22:34:24 -0300

    sync

--- Assets/Scripts/Audio/DayNightManager.cs
+++ Assets/Scripts/Audio/DayNightManager.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class DayNightManager : MonoBehaviour
 {
-    [SerializeField] [Range(0f, 1f)] private float time;
+    //================ SINGLETON LOGIC ================
+        public static DayNightManager instance = null;
+        public void SingletonCheck()
+        {
+            if (instance != null)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+    
+    [SerializeField] [Range(0f, 1f)] public float time;
     [SerializeField] private Material skyboxMaterial;
     [SerializeField] private Gradient ambientLightColor;
     [SerializeField] private Gradient fogColor;
 
-    void Update()
+    public void Update()
     {
         skyboxMaterial.SetFloat("_time", time);
         RenderSettings.ambientLight = ambientLightColor.Evaluate(time);
