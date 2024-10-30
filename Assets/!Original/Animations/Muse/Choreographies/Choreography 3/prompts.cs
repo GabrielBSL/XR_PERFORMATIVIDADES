e206c05f043ea4c932f806c9a8086ab8b4ff6685
TucoErthal <tucoerthal@gmail.com>
2024-10-30 12:37:07 -0300

    separated imported assets

---
+++ Assets/!Original/Animations/Muse/Choreographies/Choreography 3/prompts.cs
@@ -0,0 +1,6 @@
+/* Raise your right arm by passing your hand around your entire head and opening both arms at the end tilting your head to the left 1 
+
+
+6-2x: 6-2x-Raise your right arm by passing your hand around your entire head and opening both arms at the end tilting your head to the left (rejeicao) 3
+
+8-2x: 8-2x-Raise your right arm by passing your hand around your entire head and opening both arms at the end tilting your head to the left (bracos abrindo) 4*/
