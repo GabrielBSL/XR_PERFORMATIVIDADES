de92e8f3e4e3e75ca9a3234026853c5157262300
TucoErthal <tucoerthal@gmail.com>
2024-11-28 10:15:10 -0300

    backup

--- Assets/!Original/Scripts/Tuco/AldeiaFill.cs
+++ Assets/!Original/Scripts/Tuco/AldeiaFill.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class AldeiaFill : MonoBehaviour
 {
     [SerializeField] private float fill;
-    [SerializeField] private GameObject[] avatars;
+    [SerializeField] private Renderer[] avatarRenderers;
+    private MaterialPropertyBlock propertyBlock;
 
     // GET PLAYER MOVEMENT
+    [SerializeField] private bool debug;
     [SerializeField] private OldTransformSampler leftTransformSampler;
     [SerializeField] private OldTransformSampler rightTransformSampler;
     [SerializeField] private OldTransformSampler headTransformSampler;
     [SerializeField] private float requiredMovement;
     private float totalMovement = 0f;
+
+    void Start()
+    {
+        propertyBlock = new MaterialPropertyBlock();
+    }
 
     void Update()
     {
@@ -20,11 +27,13 @@ public class AldeiaFill : MonoBehaviour
         totalMovement += rightTransformSampler.GetDisplacement().magnitude;
         totalMovement += headTransformSampler.GetDisplacement().magnitude;
 
-        fill = totalMovement / requiredMovement;
+        if(!debug) fill = totalMovement / requiredMovement;
 
-        for(int i = 0; i < avatars.Length; i += 1)
+        for(int i = 0; i < avatarRenderers.Length; i += 1)
         {
-            avatars[i].GetComponent<Renderer>().material.SetFloat("_completion", Mathf.Clamp(fill - i, 0, 1));
+            propertyBlock.Clear();
+            propertyBlock.SetFloat("_completion", Mathf.Clamp(fill - i, 0, 1));
+            avatarRenderers[i].SetPropertyBlock(propertyBlock);
         }
     }
 }
